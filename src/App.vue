67414ea24b98ec6e2eadf12655f072bc5e6c5e62
ulyanovskk <mr1english4@gmail.com>
2021-11-22 17:46:16 +0300

    little better

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,73 @@
 <template>
-  <h1>SHIT</h1>
+  <nav class="bg-white py-2 md:py-4">
+    <div class="container px-4 mx-auto md:flex md:items-center">
+      <div
+        class="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0"
+        id="navbar-collapse"
+      >
+        <a
+          class="
+            p-2
+            font-display
+            text-lg
+            md:mx-2
+            text-white
+            rounded
+            bg-blue-600
+            hover:bg-blue-800
+          "
+          >Home</a
+        >
+        <a
+          class="
+            p-2
+            font-display
+            text-lg
+            md:mx-2
+            text-gray-600
+            rounded
+            hover:bg-gray-200 hover:text-gray-700
+            transition-colors
+            duration-300
+          "
+          >Lab</a
+        >
+        <a
+          class="
+            p-2
+            font-display
+            text-lg
+            md:mx-2
+            text-gray-600
+            rounded
+            hover:bg-gray-200 hover:text-gray-700
+            transition-colors
+            duration-300
+          "
+          >About</a
+        >
+      </div>
+    </div>
+  </nav>
   <div class="grid md:grid-flow-col grid-cols-4 grid-rows-1 gap-4">
-    <div class="bg-yellow-300">1
+    <div class="max-h-screen">
+      <div class="my-8">
+        <span class="font-display text-4xl font-bold text-blue-600">form</span>
+      </div>
+      <div class="bg-yellow-300 p-6 max-h-screen">
+        <img
+          class="mx-auto"
+          src="https://img.tineye.com/flickr-images/?filepath=labs-flickr-public/images/82/4725096544_824f727b53_m.jpg&size=101&size=1000"
+          alt="sunflower"
+        />
+        <div class="my-96"></div>
+      </div>
+    </div>
+    <div class="max-h-screen bg-blue-600">
       <h1 v-for="el in list" :key="el">
         {{ el }}
       </h1>
     </div>
-    <div class="max-h-screen bg-blue-600">2</div>
     <div>3</div>
     <div class="max-h-screen border-8">4</div>
   </div>

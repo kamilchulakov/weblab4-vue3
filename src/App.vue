c5dc07f9188931e95ad7dd044cfe77dcb5ff3c44
ulyanovskk <mr1english4@gmail.com>
2021-11-22 18:29:40 +0300

    little bit better

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@
   </nav>
   <div class="grid md:grid-flow-col grid-cols-4 grid-rows-1 gap-4">
     <div class="max-h-screen">
-      <div class="my-8">
+      <div class="my-8 text-center">
         <span class="font-display text-4xl font-bold text-blue-600">form</span>
       </div>
       <div class="bg-yellow-300 p-6 max-h-screen">
@@ -63,13 +63,94 @@
         <div class="my-96"></div>
       </div>
     </div>
-    <div class="max-h-screen bg-blue-600">
+    <div class="max-h-screen bg-blue-600 p-6">
+      <form class="shadow-md rounded pt-6 pb-8 mb-4 bg-blue-600">
+        <span class="text-white font-body font-bold">Type X</span>
+        <input
+          class="
+            mb-4
+            block
+            w-full
+            text-center
+            border-4 border-gray-300
+            text-gray-900
+            focus:outline-none focus:ring-gray-500 focus:border-gray-500
+            font-display font-bold
+            text-lg
+            rounded-md
+          "
+        />
+        <span class="text-white font-body font-bold">Type Y</span>
+        <input
+          class="
+            mb-4
+            block
+            w-full
+            text-center
+            border-4 border-gray-300
+            text-gray-900
+            focus:outline-none focus:ring-gray-500 focus:border-gray-500
+            font-display font-bold
+            text-lg
+            rounded-md
+          "
+        />
+        <span class="text-white font-body font-bold">Type R</span>
+        <input
+          class="
+            mb-4
+            block
+            w-full
+            text-center
+            border-4 border-gray-300
+            text-gray-900
+            focus:outline-none focus:ring-gray-500 focus:border-gray-500
+            font-display font-bold
+            text-lg
+            rounded-md
+          "
+        />
+        <button class="inline-flex items-center py-2 px-4
+            border border-transparent
+            shadow-sm
+            text-sm
+            leading-4
+            font-display font-bold
+            text-xl
+            rounded-full
+            text-white
+            bg-gray-600
+            hover:bg-gray-700
+            transition-colors
+            duration-300
+            focus:outline-none
+            focus:ring-2
+            focus:ring-offset-2
+            focus:ring-gray-500
+          "
+          type="button"
+        >
+          Submit
+        </button>
+      </form>
+      <img
+        class="mx-auto"
+        src="https://upload.wikimedia.org/wikipedia/commons/d/d9/%D0%A7%D1%91%D1%80%D0%BD%D1%8B%D0%B9_%D1%81%D1%83%D0%BF%D1%80%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9_%D0%BA%D0%B2%D0%B0%D0%B4%D1%80%D0%B0%D1%82._1915._%D0%93%D0%A2%D0%93.png"
+        alt="malevich"
+      />
+    </div>
+    <div>
       <h1 v-for="el in list" :key="el">
         {{ el }}
       </h1>
     </div>
-    <div>3</div>
-    <div class="max-h-screen border-8">4</div>
+    <div class="max-h-screen border-8">
+      <img
+        class="mx-auto"
+        src="https://media.geeksforgeeks.org/wp-content/uploads/20200525212003/About-Origin.png"
+        alt="malevich"
+      />
+    </div>
   </div>
 </template>
 
@@ -85,13 +166,3 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

290dff2d1693a8e7de8b5b343ed91535cbc04e93
ulyanovskk <mr1english4@gmail.com>
2021-11-28 17:32:06 +0300

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     fontFamily: {
       display: ["Oswald"],
       body: ["Open Sans"],
-    }
+    },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'form-color': "#e76f3d",
+      'empty-block': "#00a7c7",
+      'table-color': "#9bcfe0",
+      'mate-color': "#feab6b",
+      'not-white': "#f0eeef"
+    }),
+    textColor: theme => ({
+      ...theme('colors'),
+      'empty-text-color': "#e76f3d",
+    }),
   },
   variants: {
     extend: {},

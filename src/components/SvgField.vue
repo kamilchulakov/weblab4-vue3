56816baf0fb5a29e0ce9429adbcb83e686df6826
ulyanovskk <mr1english4@gmail.com>
2021-11-28 19:21:38 +0300

    Now close to nice SVG

--- src/components/SvgField.vue
+++ src/components/SvgField.vue
@@ -1,58 +1,73 @@
 <template>
   <svg class="" :height="svgSize + 'px'" :width="svgSize + 'px'">
     // To-Do: refactor this to computed array join
-    <polygon fill="#feab6b" fill-opacity="1" :points="svgHalf+','+(svgHalf+svgR)+' '+(svgHalf+svgR*2)+','+svgHalf+' '+svgHalf+','+svgHalf"/>
-    <rect fill="#feab6b" fill-opacity="1" :x="svgHalf" :y="svgR" :height="svgR*2" :width="svgR*2"/>
-    <g :transform="'translate(' + svgHalf + ','+ svgHalf + ')'">
-      <path d="M0 0 -50 0 A50 50 0 0 1 0 -50" fill="#feab6b"/>
+    <polygon
+      fill="#feab6b"
+      fill-opacity="1"
+      :points="
+        svgHalf +
+        ',' +
+        (svgHalf + svgR) +
+        ' ' +
+        (svgHalf + svgR * 2) +
+        ',' +
+        svgHalf +
+        ' ' +
+        svgHalf +
+        ',' +
+        svgHalf
+      "
+    />
+    <rect
+      fill="#feab6b"
+      fill-opacity="1"
+      :x="svgHalf"
+      :y="svgR"
+      :height="svgR * 2"
+      :width="svgR * 2"
+    />
+    <g :transform="'translate(' + svgHalf + ',' + svgHalf + ')'">
+      <path d="M0 0 -50 0 A50 50 0 0 1 0 -50" fill="#feab6b" />
     </g>
     <line stroke="black" x1="0" x2="300" y1="150" y2="150"></line>
     <line stroke="black" x1="150" x2="150" y1="0" y2="300"></line>
 
-    <text x="275" y="143">R</text>
-    <line x1="270" x2="270" y1="148" y2="152"></line>
-
-    <text x="215" y="143">R/2</text>
-    <line x1="210" x2="210" y1="148" y2="152"></line>
-
-    <text x="90" y="143">-R/2</text>
-    <line x1="90" x2="90" y1="148" y2="152"></line>
-
-    <text x="30" y="143">-R</text>
-    <line x1="30" x2="30" y1="148" y2="152"></line>
-
-    <text x="150" y="26">R</text>
-    <line x1="148" x2="152" y1="30" y2="30"></line>
-
-    <text x="150" y="86">R/2</text>
-    <line x1="148" x2="152" y1="90" y2="90"></line>
-
-    <text x="150" y="210">-R/2</text>
-    <line x1="148" x2="152" y1="210" y2="210"></line>
-
-    <text x="150" y="280">-R</text>
-    <line x1="148" x2="152" y1="270" y2="270"></line>
+    <SvgLineWithText :x1="250" :x2="250" :y1="148" :y2="152" text="R" />
+    <SvgLineWithText :x1="200" :x2="200" :y1="148" :y2="152" text="R/2" />
+    <SvgLineWithText :x1="100" :x2="100" :y1="148" :y2="152" text="-R/2" />
+    <SvgLineWithText :x1="50" :x2="50" :y1="148" :y2="152" text="-R" />
+    <SvgLineWithText :x1="148" :x2="152" :y1="50" :y2="50" text="R" />
+    <SvgLineWithText :x1="148" :x2="152" :y1="100" :y2="100" text="R/2" />
+    <SvgLineWithText text="-R/2" :y2="200" :y1="200" :x2="152" :x1="148" />
+    <SvgLineWithText :x1="148" :x2="152" :y1="250" :y2="250" text="-R" />
 
     <circle r="2" stroke="black" cx="150" cy="150"></circle>
 
-
-    <polygon fill="black" points="300,150 295,145 295,155" stroke="black"></polygon>
-    <polygon fill="black" points="150,0  145,5   155,5" stroke="black"></polygon>
-    <slot>
-
-    </slot>
+    <polygon
+      fill="black"
+      points="300,150 295,145 295,155"
+      stroke="black"
+    ></polygon>
+    <polygon
+      fill="black"
+      points="150,0  145,5   155,5"
+      stroke="black"
+    ></polygon>
+    <slot></slot>
   </svg>
 </template>
 
 <script>
+import SvgLineWithText from "@/components/SvgLineWithText";
 export default {
   name: "SvgField",
+  components: { SvgLineWithText },
   props: {
     svgSize: {
       type: Number,
       required: true,
       default: 300,
-    }
+    },
   },
 
   computed: {
---
+++ src/components/SvgLineWithText.vue
@@ -0,0 +1,47 @@
+<template>
+  <text :x="xForText" :y="yForText">{{ text }}</text>
+  <line stroke="black" :x1="x1" :x2="x2" :y1="y1" :y2="y2"></line>
+</template>
+
+<script>
+export default {
+  name: "SvgLineWithText",
+  data() {
+    return {
+      formattingNumber: 5,
+    }
+  },
+  props: {
+    x1: {
+      type: Number,
+      required: true,
+    },
+    x2: {
+      type: Number,
+      required: true,
+    },
+    y1: {
+      type: Number,
+      required: true,
+    },
+    y2: {
+      type: Number,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      default: "",
+    },
+  },
+
+  computed: {
+    xForText() {
+      return this.x2 + this.formattingNumber;
+    },
+    yForText() {
+      return this.y2 - this.formattingNumber;
+    },
+  },
+};
+</script>

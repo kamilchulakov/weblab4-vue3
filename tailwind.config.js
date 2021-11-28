6dc400efa372df2aff2f608d568f7349b405296a
ulyanovskk <mr1english4@gmail.com>
2021-11-28 18:29:06 +0300

    Add colors for fill

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,14 @@ module.exports = {
       'mate-color': "#feab6b",
       'not-white': "#f0eeef"
     }),
+    fill: theme => ({
+      ...theme('colors'),
+      'form-color': "#e76f3d",
+      'empty-block': "#00a7c7",
+      'table-color': "#9bcfe0",
+      'mate-color': "#feab6b",
+      'not-white': "#f0eeef"
+    }),
     textColor: theme => ({
       ...theme('colors'),
       'empty-text-color': "#e76f3d",
